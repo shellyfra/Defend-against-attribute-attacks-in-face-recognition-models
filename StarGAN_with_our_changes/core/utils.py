debb227452a29bce00a87d1cbacf761d5ce8d3b8
gil-kapel <74864454+gil-kapel@users.noreply.github.com>
2023-01-14 21:42:07 +0200

    Update utils.py

--- StarGAN_with_our_changes/core/utils.py
+++ StarGAN_with_our_changes/core/utils.py
@@ -17,11 +17,11 @@ def save_image(x, ncol, filename):
 def translate_using_reference(nets, args, x_src, y_src, x_ref, y_ref, filename):
     N, C, H, W = x_src.size()
     for j, (x_s, y_s) in enumerate(zip(x_src, y_src)):
-        x_s = torch.from_numpy(np.expand_dims(x_s, axis=0))
+        x_s = torch.unsqueeze(x_s, dim=0)
         for i, (x_r, y_r) in enumerate(zip(x_ref, y_ref)):
-            x_r = torch.from_numpy(np.expand_dims(x_r, axis=0))
-            y_r = torch.from_numpy(np.expand_dims(y_r, axis=0))
+            x_r = torch.unsqueeze(x_r, dim=0)
+            y_r = torch.unsqueeze(y_r, dim=0)
             masks = nets.fan.get_heatmap(x_s) if args.w_hpf > 0 else None
             s_ref = nets.style_encoder(x_r, y_r)
             x_fake = nets.generator(x_s, s_ref, masks=masks)
-            save_image(x_fake, N + 1, f'{filename}/{y_s}/new_image_{i}.jpg')
+            save_image(x_fake, N + 1, f'{filename}/{y_s}/new_image_{y_s}.jpg')
